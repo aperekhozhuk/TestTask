36828b5c96e0b4f3170092a6dc40adb50794560a
Anton Perekhozhuk <anton.perekhozhuk@gmail.com>
2021-02-18 02:22:43 +0200

    Add UnLike view

--- project/api/views.py
+++ project/api/views.py
@@ -42,8 +42,8 @@ class PostViewSet(
 class LikeView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, post_id, format=None):
-        user = request.user
+    @staticmethod
+    def get_post_or_404_response(post_id):
         # Checking if Post exists:
         try:
             post = Post.objects.get(id=post_id)
@@ -51,12 +51,33 @@ class LikeView(APIView):
             return Response(
                 {"error": "Post Not Found"}, status=status.HTTP_404_NOT_FOUND
             )
+        return post
+
+    def post(self, request, post_id, format=None):
+        post = self.get_post_or_404_response(post_id)
+        if isinstance(post, Response):
+            return post
         # Trying to put like:
         try:
-            Like.objects.create(post=post, user=user)
+            Like.objects.create(post=post, user=request.user)
         except IntegrityError:
             return Response(
                 {"error": "You already liked this post!"},
                 status=status.HTTP_403_FORBIDDEN,
             )
         return Response({"message": "Post liked!"}, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, post_id, format=None):
+        post = self.get_post_or_404_response(post_id)
+        if isinstance(post, Response):
+            return post
+        # Checking if user liked this post before
+        try:
+            like = Like.objects.get(post=post, user=request.user)
+        except Like.DoesNotExist:
+            return Response(
+                {"error": "You didn't like this post!"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        like.delete()
+        return Response({"message": "Post unliked!"}, status=status.HTTP_200_OK)
